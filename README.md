c5008e1410f34d46dae9c2b4b68d7e6add3a999b
Katya Baranovskaya <blondibiznes@gmail.com>
2022-02-13 23:52:31 +0300

    add timer and modal

--- README.md
+++
@@ -1,6 +0,0 @@
-# tabs
-https://blondibiznes.github.io/tabs/tabs1/
-
-https://blondibiznes.github.io/tabs/tabs2/
-
-https://blondibiznes.github.io/tabs/tabs3/
--- tabs1/script.js
+++ tabs1/script.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Timer 
 
-    let deadline = '2022-02-14';
+    let deadline = '2018-11-21';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -84,4 +84,33 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+
+    // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
 });
+
+// Второе задание
+
+// let age = document.getElementById('age');
+ 
+// function showUser(surname, name) {
+//          alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// }
+ 
+// showUser.apply(age, ["Горький","Максим"]);
